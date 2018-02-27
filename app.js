825c8290a2696f8b2872e4653eeaaf253493da7e
Adolfo Morales <gadolfomoralesmartinez@gmail.com>
2018-02-26 22:18:10 -0600

    Add cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 let db = require('./db');
+let cors = require('cors')
 
 let UserController = require('./server/controllers/user.controller');
 let PackageController = require('./server/controllers/package.controller');
@@ -8,10 +9,11 @@ let PilotController = require('./server/controllers/pilot.controller');
 let RouteController = require('./server/controllers/pilot.controller');
 let TruckController = require('./server/controllers/truck.controller');
 
-app.use('/users', UserController);
-app.use('/packages', PackageController);
-app.use('/pilots', PilotController);
-app.use('/routes', RouteController);
-app.use('/trucks', TruckController);
+app.use(cors())
+app.use('/user', UserController);
+app.use('/package', PackageController);
+app.use('/pilot', PilotController);
+app.use('/route', RouteController);
+app.use('/truck', TruckController);
 
 module.exports = app;
